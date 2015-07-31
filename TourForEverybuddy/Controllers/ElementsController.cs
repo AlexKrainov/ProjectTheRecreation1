0adf0f1b6770e40362a2d333b1546cda6a07560c
AlexKrainov <ialexkrainov2@gmail.com>
2015-07-31 18:44:35 +0300

    Проект Visual Studio

---
+++ TourForEverybuddy/Controllers/ElementsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TourForEverybuddy.Controllers
+{
+    public class ElementsController : Controller
+    {
+        // GET: Elements
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TourForEverybuddy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TourForEverybuddy.MvcApplication" Language="C#" %>
