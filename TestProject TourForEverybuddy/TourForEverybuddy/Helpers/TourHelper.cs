c83ff1f35db7b0ffa0ae67fb6e83eec95255fa55
AlexKrainov <ialexkrainov2@gmail.com>
2015-09-01 08:31:56 +0300

    New tour

--- TestProject TourForEverybuddy/TourForEverybuddy/Helpers/TourHelper.cs
+++ TestProject TourForEverybuddy/TourForEverybuddy/Helpers/TourHelper.cs
@@ -20,13 +20,36 @@ namespace TourForEverybuddy.Helpers
         /// <returns></returns>
         public static MvcHtmlString Tour(this HtmlHelper html, int id, bool edit = false)
         {
-            var tag = new TagBuilder("a");
-            tag.Attributes.Add("style", "background-color: red");
-            tag.SetInnerText("Hello");
+            var section = new TagBuilder("section"); section.AddCssClass("post");
+            var divImg = new TagBuilder("div"); divImg.AddCssClass("img");
+            var img = new TagBuilder("img"); img.Attributes.Add("id", "img"); img.Attributes.Add("src", "tra");
+            
+            var divContent = new TagBuilder("div"); divContent.AddCssClass("content");
+            
+            
 
-
-            return new MvcHtmlString(tag.ToString());
+            return new MvcHtmlString(section.ToString());
         }
 
+        /*
+         * 
+         *  <section class="post">
+            <div class="img">
+                <img id="img" src="@Url.Action("GetUrlFromPictureOfTour", "Picture", new { pictureId = Model.Tours.ElementAt(0).Tour_PictureOfTour.ElementAt(0).Id,
+                 tourID = Model.Tours.ElementAt(0).Tour_PictureOfTour.ElementAt(0).TourID })"
+                     alt="" />
+            </div>
+            <div class="content">
+                @Html.ActionLink(Model.Tours.ElementAt(0).title, "ActionName", "controllerName", null, new { @class = "titleTour" })
+                @*<b class="titleTour">Same text from title tour and text from title tour</b>*@
+                <p class="contentDescription">
+                    There's a funny blog post about how to download SQL Server Express from Long Zheng.
+                    It surprisingly how complex some companies make downloading things. I've always thought that a giant Download Now button is the best way, but perhaps that's just me?
+                </p>
+
+                @*<input class="button" type="submit" value="View" />*@
+            </div>
+        </section>
+         */
     }
 }
